2037f2e0df076aaa25950c0d408fb5475afefdb8
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-05-25 16:50:23 +0530

    First commit

---
+++ resource.ts
@@ -0,0 +1,9 @@
+/**
+ * add these all plugins: npm i react-native-snackbar @react-navigation/native axios @react-navigation/stack react-native-svg
+ *
+ */
+
+// RNScreen was not found:
+/**
+ * npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+ */
